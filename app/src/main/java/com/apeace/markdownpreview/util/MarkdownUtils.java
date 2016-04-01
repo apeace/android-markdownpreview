449211b4603cfb7ae5fb848e229e831523b0f16f
Andrew Peace <apeace@gmail.com>
2016-04-01 10:28:06 -0400

    MarkdownUtils test coverage

--- app/src/main/java/com/apeace/markdownpreview/util/MarkdownUtils.java
+++ app/src/main/java/com/apeace/markdownpreview/util/MarkdownUtils.java
@@ -2,7 +2,7 @@ package com.apeace.markdownpreview.util;
 
 import com.github.rjeschke.txtmark.Processor;
 
-public class MarkdownUtils {
+public final class MarkdownUtils {
 
     public static String markdownToHtml(String html) {
         return Processor.process(html).trim();
--- app/src/test/java/com/apeace/markdownpreview/MarkdownUtilsTest.java
+++ app/src/test/java/com/apeace/markdownpreview/MarkdownUtilsTest.java
@@ -9,11 +9,17 @@ import com.apeace.markdownpreview.util.MarkdownUtils;
 public class MarkdownUtilsTest {
 
     @Test
-    public void markdownToHtml() throws Exception {
+    public void markdownToHtml() {
         String markdown = "This is **bold**";
         String html = "<p>This is <strong>bold</strong></p>";
         String result = MarkdownUtils.markdownToHtml(markdown);
         assertEquals(html, result);
+    }
+
+    @Test
+    public void markdownConstructor() {
+        // does nothing. just here to provide coverage of constructor
+        MarkdownUtils mu = new MarkdownUtils();
     }
 
 }
